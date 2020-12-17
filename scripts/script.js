1e91b136be1ea51afa076c23355b93bbfa0898f7
ROEL2407 <roel24komen@gmail.com>
2020-12-17 16:37:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,15 @@ function openMenu() {
 var Opst = document.querySelector("#opstelling_btn");
 var game = document.querySelector("#wedstrijd_btn");
 var opstelling_info = document.querySelector("#opstelling");
-var wedstrijd_info = document.getElementsByClassName(".wedstrijd");
+var wedstrijd_info = document.querySelector("#wed-info");
+var wedstrijd_stats = document.querySelector("#wed-stats");
 
 Opst.addEventListener("click", openOpst);
 
 function openOpst() {
     opstelling_info.classList.remove("invisible");
-    wedstrijd_info.classList.Add("invisible");
+    wedstrijd_info.classList.add("invisible");
+    wedstrijd_stats.classList.add("invisible");
     Opst.classList.remove("not-selected");
     game.classList.add("not-selected");
 }
@@ -29,14 +31,16 @@ game.addEventListener("click", openWed);
 function openWed() {
     opstelling_info.classList.add("invisible");
     wedstrijd_info.classList.remove("invisible");
+    wedstrijd_stats.classList.remove("invisible");
     Opst.classList.add("not-selected");
     game.classList.remove("not-selected");
 }
 
 var tableRow = document.querySelector("#JSlink");
-
+/*
 tableRow.addEventListener("click", linkToPage);
 
 function linkToPage() {
   location.replace("wedstrijd.html")
 }
+*/
